c036e9016ebcc28de8c07e783a189c18d6c923c4
Curtis Sunley Jr <csunley@varrow.com>
2014-07-14 11:48:13 -0400

    Print completed XML tree when excel workbook is uploaded

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.get('/success', function (request, response) {
  * HTTP post request
  */
 var fullfile;
+
 app.post('/upload', function (request, response) {
     var fstream;
     request.pipe(request.busboy);
@@ -76,109 +77,98 @@ app.post('/upload', function (request, response) {
             var month = date.getMonth()+1;
             var year = date.getFullYear();
             var getDateTime = month + "-" + day + "-" + year;
-            console.log(getDateTime);
-            
                         
-                                                                           
-            //Will print out parsed excel sheet data
-            for (var i=0; i < obj.worksheets.length; i++)
-            {             
-                var myObj = obj.worksheets[i];
-                console.log(myObj.data);               
-              }
             
-            /*
-             * Creates static portion of XML sheet that will be pushed to Intacct and prints it
-             * to the console
-             */
-            var page = builder.create('request', {'version': '1.0', 'encoding': 'UTF-8'});            
-            var staticXML = {
-                '#list': [
-                            {
-                                control : {
-                                    'senderid': 'Varrow',
-                                    'password': 'KYloh3jU0W',
-                                    'controlid': 'Varrow',
-                                    'dtdversion': '2.1'
-                                        },
+            var root = builder.create('request');
             
-                                operation: {
-                                    authentication: {
-                                        login: {
-                                            'userid': 'dsgroup',
-                                            'companyid': 'Varrow',
-                                            'password': 'V@rrowDevTeam2014'
-                                                }
-                                            },
-                                    
-                                    content: {
-                                        'function': {
-                                            '@controlid': 'Varrow',
-                                            create_billbatch: {
-                                            'batchtitle': 'Conucur Batch Upload: ' + getDateTime
-                                            }
-                                     }
-                                  }
-                                }
-                            }
-                        ]
-                    };
+            var control = root.ele('control');
+            var senderid = control.ele('senderid', 'Varrow');
+            var password = control.ele('password', 'KYloh3jU0W');
+            var controlid = control.ele('controlid', 'Varrow');
+            var dtdversion = control.ele('dtdversion', '2.1');
             
-          
-            /*
-             *
-             *
-             */
-            var pageTwo = builder.create('create_bill', {'version': '1.0', 'encoding': 'UTF-8'});
-            var varrowEmployee = {
-                '#list': [
-                            {
-                                'vendorid': 'ID Here',
-                                datecreated: {
-                                    'year': 'Year Here',
-                                    'month': 'Month Here',
-                                    'day': 'Day Here'
-                                },
-                                datedue: {
-                                    'year': 'Year Here',
-                                    'month': 'Month Here', 
-                                    'day': 'Day Here'
-                                },
-                                'billno': 'Bill Number Here',
-                                'description': 'Description Here', 
-                                billitems: {
-                                    'lineitems': 'Line Item Here'
-                                }
-                            }
-                ]
-            };
+            var operation = root.ele('operation');
+            var authentication = operation.ele('authentication');
+            var login = authentication.ele('login');
+            var userid = login.ele('userid', 'dsgroup');
+            var companyid = login.ele('companyid', 'Varrow');
+            var password = login.ele('password', 'V@rrowDevTeam2014');
             
-            var pageThree = builder.create('lineitem', {'version': '1.0', 'encoding': 'UTF-8'});
-            var employeeLineItem = {
-                '#list': [
-                            {
-                                    'glaccountno': 'GL Account No Here',
-                                    'amount': 'Amount Here',
-                                    'memo': 'Memo Here',
-                                    'departmentid': 'Department Id Here'
-                            }
-                ]
-            };
+            var content = operation.ele('content');
+            var fnctn = content.ele('function').att('controlid', 'Varrow');
+            var createbillbatch = fnctn.ele('create_billbatch');
+            var batchtitle = createbillbatch.ele('Concur Batch Upload: ' + getDateTime);
+            
+            for (var key in obj.worksheets[0]) {
+                 //Get the value of the worksheet
+                 if (obj.worksheets[0].hasOwnProperty(key)){
+                    var val = obj.worksheets[0][key];
+                    //Employee list for tracking duplicates
+                    var empNames = [];
+                     //Start loop at one to skip first row of headers
+                    for (var i = 1; i < val.length; i++){
+                        //Row objects start here
+                        var row = val[i];
+                        var createBill;
                         
-                                        
-                                        
-                                            
-            page.ele(staticXML);
-            pageTwo.ele(varrowEmployee);
-            pageThree.ele(employeeLineItem);
-            console.log(page.toString({ pretty: true }));
-            console.log(pageTwo.toString({pretty: true}));
-            console.log(pageThree.toString({pretty: true}));
-    
+                        //Check to see if empty object from excel parser
+                        if(row[0]['value'] != undefined){
+                            //Items we need from excel parser
+                            var empNameStr = row[0]['value'].toString();
+                            var vendoridStr = row[1]['value'].toString();
+                            var billnoStr = row[10]['value'].toString();
+                            var descriptionStr = row[10]['value'].toString();
+                            var glaccountnoStr = row[2]['value'].toString(); + '-000';
+                            var amountStr = row[3]['value'].toString();
+                            var memoStr = row[4]['value'].toString() + ' : ' + row[6]['value'].toString();
+                            var departmentidStr = row[7]['value'].toString();
+                            
+                            //If this is a new employee, create first set of items
+                            if (empNames.indexOf(empNameStr)<0){
+                                createBill = batchtitle.ele('create_bill');
+                                //Push the name onto empNames so that employee's are not duplicated
+                                empNames.push(empNameStr);
+                                
+                                createBill.ele('vendorid', vendoridStr);
+                                
+                                var datecreated = createBill.ele('datecreated');
+                                datecreated.ele('year', year);
+                                datecreated.ele('month', month);
+                                datecreated.ele('day', day);
+                                
+                                createBill.ele('billno', billnoStr);
+                                createBill.ele('description', descriptionStr);
+                                
+                                var billItems = createBill.ele('billitems');
+                                var lineItem = billItems.ele('lineitem');
+                                lineItem.ele('glaccountno', glaccountnoStr);
+                                lineItem.ele('amount', amountStr);
+                                lineItem.ele('memo', memoStr);
+                                lineItem.ele('departmentid', departmentidStr);
+                            }
+                            //If employee exists, only add additional line items
+                            else {
+                                var lineItem = billItems.ele('lineitem');
+                                lineItem.ele('glaccountno', glaccountnoStr);
+                                lineItem.ele('amount', amountStr);
+                                lineItem.ele('memo', memoStr);
+                                lineItem.ele('departmentid', departmentidStr);
+                            }
+                        } else {
+                            //Row did not contain valid data.
+                        }
+                    }
+                }
+            }
+                   console.log(root.toString({pretty:true}));
         });
     });
 });
+            
 
+            
+                               
+            
 
 
 
